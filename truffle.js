f978d899127356c5dbaef25daa2a7e45ffe95541
lzxm160 <lzxm160@sina.com>
2018-12-19 12:04:07 +0800

    first commit

--- truffle.js
+++ truffle.js
@@ -21,8 +21,8 @@ module.exports = {
       // host: '172.16.2.12',
       port: 18545,
       network_id: '*', // Match any network id
-      gas:100000000,
-      gasPrice: 50000000000,
+      gas: 4500000,
+      gasPrice: 10000000000,
     }
   },
   compilers: {
